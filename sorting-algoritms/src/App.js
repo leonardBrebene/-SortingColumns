8cd0376e358dc07f5d6c0d6adebb437f21ce717b
leonardBrebene <brebene.leonard@yahoo.ro>
2021-03-06 22:04:02 +0200

    Setting columns and space between them

--- sorting-algoritms/src/App.js
+++ sorting-algoritms/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import React, { useState, useEffect } from 'react'
+import { ProgressBar } from 'react-bootstrap'
 
 function App() {
 
   const [l, setl] = useState(0);
   const [columnArray, setColumnArray] = useState([]);
+  const [numberOfColumns,setNumberColumns]=useState(10);
 
 
   useEffect(() => {
@@ -15,34 +17,48 @@ function App() {
       k++;
       setl(k);
     }
-    
+
     setTimeout(() => altceva(), 1000);
   }, [l])
 
   useEffect(() => {
     function GetArray() {
       const array = [];
-      for (let i = 0; i < 100; i++) {
-        array.push(randomIntBetween(5, 30))
+      for (let i = 0; i < numberOfColumns; i++) {
+        array.push(randomIntBetween(1, 100))
       }
       setColumnArray(array);
     }
+    function handleSubmit(e){
+      e.preventDefault();
+    }
 
-    setTimeout(() => GetArray(), 10000);
-  }, [columnArray])
-
-
-
+    setTimeout(() => GetArray(), 5000);
+  }, [columnArray,numberOfColumns])
 
 
   function randomIntBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+  //Math.floor used to parse numbers to integer
 
   return (
     <div className="App">
-      hello world {l} {columnArray.map(value => (<li>{value}</li>))}
+      <div >ceva</div>
+      <div className='form-control'>
+          <label>Number of columns</label>
+          <input type="text" id="lastName" name="lastName"
+            value={numberOfColumns} onChange={(e) => setNumberColumns(e.target.value)} />
+        </div>
+        <button id="saveContact" type="submit" >Save</button>
+      hello world <div className='arrayBar'>{l}</div>
+      <p></p>
+      <div className='arrayBarContainer'>
+        {columnArray.map( value => (
+        <div className="arrayBar" style={{height:`${value*0.8}vh`,width:`${85/numberOfColumns}%`,margin:`0 ${14.8/numberOfColumns}% 0 0`} }></div>))}
+      </div>
     </div>
+
   );
 }
 export default App;
