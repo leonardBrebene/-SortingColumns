8ee918a17afaab432da804161ea6c947f04c3951
leonardBrebene <brebene.leonard@yahoo.ro>
2021-03-06 08:03:25 +0200

    Using UseEffect function to rerender components

--- sorting-algoritms/src/App.js
+++ sorting-algoritms/src/App.js
@@ -5,28 +5,35 @@ import React, { useState, useEffect } from 'react'
 function App() {
 
   const [l, setl] = useState(0);
-  const [columnArray,setColumnArray]=useState([]);
+  const [columnArray, setColumnArray] = useState([]);
 
 
   useEffect(() => {
-    setTimeout(() => altceva(), 1000);
-    setTimeout(()=>GetArray(),10000);
-  })
-
-  var k = 0;
-  function altceva() {
-    k = l;
-    k++;
-    setl(k);
-  }
-
-  function GetArray() {
-    const array = [];
-    for (let i = 0; i < 100; i++) {
-      array.push(randomIntBetween(5, 30))
+    var k = 0;
+    function altceva() {
+      k = l;
+      k++;
+      setl(k);
     }
-    setColumnArray(array);
-  }
+    
+    setTimeout(() => altceva(), 1000);
+  }, [l])
+
+  useEffect(() => {
+    function GetArray() {
+      const array = [];
+      for (let i = 0; i < 100; i++) {
+        array.push(randomIntBetween(5, 30))
+      }
+      setColumnArray(array);
+    }
+
+    setTimeout(() => GetArray(), 10000);
+  }, [columnArray])
+
+
+
+
 
   function randomIntBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -34,7 +41,7 @@ function App() {
 
   return (
     <div className="App">
-      hello world {l} {columnArray.map(value=>(<li>{value}</li>))}
+      hello world {l} {columnArray.map(value => (<li>{value}</li>))}
     </div>
   );
 }
