3d478fe2ee9272d703c2d1f2d04e48326344b418
leonardBrebene <brebene.leonard@yahoo.ro>
2021-03-10 23:19:23 +0200

    Tri to implement bubblesort

--- sorting-algoritms/src/App.js
+++ sorting-algoritms/src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 import React, { useState, useEffect } from 'react'
 
 function App() {
-
   const [l, setl] = useState(0);
   const [columnArray, setColumnArray] = useState([]);
   const [numberOfColumns, setNumberColumns] = useState(10);
-  const [tempNum,setTempNum]=useState(10);
+  const [tempNum, setTempNum] = useState(10);
 
-  useEffect(() => {
-    var k = 0;
-    function altceva() {
-      k = l;
-      k++;
-      setl(k);
-    }
-    setTimeout(() => altceva(), 1000);
-  }, [l])
 
   useEffect(() => {
     const array = [];
@@ -26,15 +16,35 @@ function App() {
     }
     setColumnArray(array);
 
-    console.log('number of columns'+ numberOfColumns)
+    console.log('number of columns' + numberOfColumns)
   }, [numberOfColumns])
-   
+
+  function Bubble() {
+    console.log("a fost apelat bubble")
+    let columnarray = columnArray;
+    let numberofcolumns = numberOfColumns;
+    for (let i = numberofcolumns - 1; i > -1; i--) {
+      for (let j = 0; j < i; j++) {
+
+        if (columnarray[j] > columnarray[j + 1]) {
+         // setTimeout(() => {
+            let k = columnarray[j];
+            columnarray[j] = columnarray[j + 1];
+            columnarray[j + 1] = k
+         // }, 1000 * j);
+        }
+      }
+    }
+    console.log(columnarray)
+  }
+
 
   function onClick(e) {
     e.preventDefault();
-    setNumberColumns(tempNum); 
+    setNumberColumns(tempNum);
+
   }
-  function getArray(){
+  function getArray() {
     const array = [];
     for (let i = 0; i < numberOfColumns; i++) {
       array.push(randomIntBetween(1, 100))
@@ -49,19 +59,24 @@ function App() {
 
   return (
     <div className="App">
-      hello world <div className='arrayBar'>{l}</div> <p/>
+      hello world <div className='arrayBar'>{l} </div>  <p />
 
       <form className='form-control' >
         <label>Set number of columns</label>
-        <input type="text" id="numberOfColumns"  value={tempNum} onChange={(e)=>setTempNum(e.target.value)} />
+        <input type="text" id="numberOfColumns" value={tempNum} onChange={(e) => setTempNum(e.target.value)} />
         <button id="numberOfColumnsButton" type="submit" onClick={onClick}  >Set</button>
       </form>
-     
-     <button onClick={getArray}>Reset values</button> <p/>
 
+      <button onClick={getArray}>Reset values</button> <p />
+      <button onClick={Bubble}>Bubble </button>
       <div className='arrayBarContainer'>
-        {columnArray.map(value => (
-          <div className="arrayBar" style={{ height: `${value * 0.8}vh`, width: `${85 / numberOfColumns}%`, margin: `0 0 0 ${14.8 / numberOfColumns}%` }}></div>))}
+        {columnArray.map((value, index) => (
+          <div className="arrayBar" id={index} style={{
+            height: `${value * 0.8}vh`, width: `${85 / numberOfColumns}%`,
+            margin: `0 0 0 ${14.8 / numberOfColumns}%`
+          }}>
+          </div>))}
+
       </div>
     </div>
 
